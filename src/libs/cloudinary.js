e6382cb5f1c76f59eec6010e86a463a437cc06d8
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-11-22 15:51:07 -0500

    config cloudinary

--- src/libs/cloudinary.js
+++ src/libs/cloudinary.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import { API_SECRET, CLOUD_NAME, API_KEY } from "../config";
+import { API_SECRET, CLOUD_NAME, API_KEY } from "../config.js";
 cloudinary.config({
   cloud_name: CLOUD_NAME,
   api_key: API_KEY,
@@ -8,6 +8,12 @@ cloudinary.config({
 export const uploadImage = async (filePath) => {
   return await cloudinary.uploader.upload(filePath, {
     folder: "avatars",
+  });
+};
+
+export const uploadFile = async (filePath) => {
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "Files",
   });
 };
 
