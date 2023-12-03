62580acb34455366c59aa753e3f05641dcdffc47
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-12-03 17:02:37 -0500

    token

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -87,6 +87,7 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
+      httpOnly: true,
       domain: ".onrender.com",
       sameSite: "None",
       secure: true,
