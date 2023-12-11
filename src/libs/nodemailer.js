e363dec46f60aa433f20d0d2c1460a11c226bc7c
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-12-11 18:27:24 -0500

    update mailer

--- src/libs/nodemailer.js
+++ src/libs/nodemailer.js
@@ -2,10 +2,14 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: "iiue2024@gmail.com",
     pass: "omkp iwmy zmzj vbhd",
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  debug: true,
 });
